240fb58aad0dd6e4c823236cc31c06ac907006fd
Souravdey777 <piyush.kolkata@gmail.com>
2021-03-18 01:14:06 +0530

    footer added

--- src/components/Apod/Apod.js
+++ src/components/Apod/Apod.js
@@ -4,41 +4,41 @@ import logo from "../../assets/images/apod logo.svg"
 
 
 const Apod = (props) => {
-  const [XY,setXY]= useState({
-    x:0,
-    y:0
+  const [XY, setXY] = useState({
+    x: 0,
+    y: 0
   })
-  const [sXY,setsXY]= useState({
-    x:0,
-    y:0
+  const [sXY, setsXY] = useState({
+    x: 0,
+    y: 0
   })
   var selectRef = React.createRef();
 
-  const _onMouseMove=(e)=> {
+  const _onMouseMove = (e) => {
     setXY({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY })
-    var sxPos = (e.nativeEvent.offsetX / selectRef.current.clientWidth*100 - 50)*2;
-	  var syPos = (e.nativeEvent.offsetY / selectRef.current.clientHeight*100 - 50)*2;
-    setsXY({x:(0.03*3*sxPos),y:(-0.03*3*syPos)})
-    console.log(XY.x,XY.y)
+    var sxPos = (e.nativeEvent.offsetX / selectRef.current.clientWidth * 100 - 50) * 2;
+    var syPos = (e.nativeEvent.offsetY / selectRef.current.clientHeight * 100 - 50) * 2;
+    setsXY({ x: (0.1* sxPos), y: (-0.1* syPos) })
+    console.log(XY.x, XY.y)
   };
-  const { title,copyright, date, hdurl, explanation } = props.data
+  const { title, copyright, date, hdurl, explanation } = props.data
   console.log(props.data)
   return (
     <div className={style.rootAPOD}>
       <img className={style.logo} src={logo} alt="The NASA APOD Project" />
       <div className={style.line} />
       <div className={style.Apod}>
-        <div className={style.bline} />
+        {/* <div className={style.bline} /> */}
         <div className={style.title}>{title}</div>
         <div className={style.date}>{date}</div>
         <div style={{ display: "flex", flexFlow: "column" }}>
           <div className={style.image} ref={selectRef}
-            style={{transform: `rotateX(${sXY.x}deg) rotateY(${sXY.y}deg)`}}
-             onMouseOut={()=>setsXY({ x: 0, y: 0 })} 
+            style={{ transform: `rotateX(${sXY.x}deg) rotateY(${sXY.y}deg)` }}
+            onMouseOut={() => setsXY({ x: 0, y: 0 })}
             onMouseMove={_onMouseMove.bind(this)} >
             <img className={style.Apodimage} alt={title} src={hdurl} />
-            </div>
-            <div>by {copyright}</div>
+          </div>
+          <div>by {copyright}</div>
           <div className={style.explanation}>{explanation}</div>
         </div>
       </div>
--- src/containers/NasaApod.js
+++ src/containers/NasaApod.js
@@ -8,14 +8,15 @@ const NasaApod = () => {
   const [data, setdata] = useState();
 
   useEffect(() => {
-    if(!data)
-    axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=xU3CMrS3nNgv0djoupzpT1078bczKb8CPqYyZAZN`)
-      // .get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.APOD_KEY}`)
-      .then(res => {
-        setdata(res.data)
-      })
-      .catch(error => { setdata({ title: "Hi this is a dummy text for Title", date: 20, hdurl: "https://media.istockphoto.com/vectors/starry-sky-seamless-pattern-white-and-blue-dots-in-galaxy-and-stars-vector-id904278188?b=1&k=6&m=904278188&s=612x612&w=0&h=hUSezsk0k49FT7m14ClXGEX07PpT8_cg4dMVck2F1cM=", explanation: "kjhkfhioehf" }) })
+    if (!data)
+      axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=xU3CMrS3nNgv0djoupzpT1078bczKb8CPqYyZAZN`)
+        // .get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.APOD_KEY}`)
+        .then(res => {
+          setdata(res.data)
+
+        })
+        .catch(error => { setdata({ title: "Hi this is a dummy text for Title", date: 20, hdurl: "https://media.istockphoto.com/vectors/starry-sky-seamless-pattern-white-and-blue-dots-in-galaxy-and-stars-vector-id904278188?b=1&k=6&m=904278188&s=612x612&w=0&h=hUSezsk0k49FT7m14ClXGEX07PpT8_cg4dMVck2F1cM=", explanation: "kjhkfhioehf" }) })
   });
 
   return (
@@ -23,7 +24,51 @@ const NasaApod = () => {
       {data ?
         <div className={styles.NasaAPOD}>
           <Apod data={data} />
-          <footer className={styles.footer}></footer>
+          <footer className={styles.footer}>
+              <div style={{textAlign:"left"}}>
+                <b>© {new Date().getFullYear()} The NASA APOD Project</b>
+                <div>This Project is powered by NASA Astronomy Picture of the Day</div>
+              </div>
+              <div>
+                Developed with{" "}
+                <span role="img" aria-label="fire">
+                  ❤️
+            </span>{" "}
+            by{" "}
+                <a
+                  href="https://github.com/Souravdey777/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.developer}
+                >
+                  @Souravdey777
+            </a>
+                <br />
+                <div className={styles.social_icons}>
+                  <a
+                    href="https://github.com/Souravdey777/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fab fa-github"></i>
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/souravdey777/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fab fa-linkedin"></i>
+                  </a>
+                  <a
+                    href="https://twitter.com/Souravdey777/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                </div>
+              </div>
+          </footer>
         </div>
         : <div />}
     </>
